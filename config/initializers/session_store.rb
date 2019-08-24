7bec2a1a87080aceb534fdeb340ddb89ef4eac71
ttran9 <toddtran9@gmail.com>
2019-08-24 01:48:02 -0700

    Modifying the configuration to ensure the cookie can be set in both production and development environments.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,8 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "tt-authentication-app-api.herokuapp.com"
+if Rails.env == "production"
+  # with domain set, the browser expects the cookie to be set from the specified domain.
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "tt-authentication-app-api.herokuapp.com"
+else
+  # since we are not setting the cookie from the heroku app in the development environment we want to remove
+  # the domain portion.
+  Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
