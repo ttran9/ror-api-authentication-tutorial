b1a31739592cd894d382bc92831fdafb1cb19521
ttran9 <toddtran9@gmail.com>
2019-08-24 01:42:28 -0700

    Adding extra routes to process registration, logout, and log in requests.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  resources :sessions, only: [create] # allow us to have access to session.
+  resources :sessions, only: [:create] # allow us to have access to session.
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logged_in, to: "sessions#logged_in"
   root to: "static#home"
 end
