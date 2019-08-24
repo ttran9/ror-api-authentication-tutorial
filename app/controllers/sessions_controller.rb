abbe5399d2d2fd0cacc6c06206f7a68fdd5fb467
ttran9 <toddtran9@gmail.com>
2019-08-23 19:16:04 -0700

    Creating a controller to modify the session upon successful authentication.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  def create
+    # We have a user model which we find by email
+    # params[user][email] is what the front-end sends because it wraps up a User domain object with an email field.
+    # it is possible we do not wrap the sent form contents into a User object and where we just send the email field
+    # for the try authenticate, this is a method built into rails and it can be used because in user.rb we stated
+    # "has_secure_password"
+    # so we try to find a user with the passed in email and then we try to verify the password.
+    # if all of this works then we store the user into this user variable.
+    user = User.
+              find_by(email: params["user"]["email"])
+               .try(:authenticate, params["user"]["password"])
+    if user
+      # in the http cookie we will send back the retrieved user's database id.
+      # this id that is returned is encrypted and sent back to the user's browser.
+      session[:user_id] = user.id
+      render json: {
+          status: :created,
+          logged_in: true,
+          user: user
+      }
+    else
+      render json: {
+          status: 401 # unauthorized
+      }
+    end
+  end
+end
